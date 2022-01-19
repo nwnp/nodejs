214c25c9850736eb9b67511d386942c5de26ff33
gzn_jjw <jjw_0418@naver.com>
2022-01-19 16:21:59 +0900

    feat: 백엔드 연습

---
+++ self-study/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const fs = require("fs");
+
+const userRouter = express.Router();
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res, next) => {
+  res.send("root - get");
+});
+
+userRouter.param("id", (req, res, next, value) => {
+  console.log(value);
+  next();
+});
+
+userRouter.get("/", (req, res, next) => {
+  res.send("User info with ID");
+});
+
+userRouter.get("/:id", (req, res, next) => {
+  res.send(req.params.id);
+});
+
+userRouter.post("/", (req, res, next) => {
+  res.send("register user");
+});
+
+userRouter.post("/:id", (req, res, next) => {
+  const userId = req.params.id;
+  res.send(`id: ${userId} - success register`);
+});
+
+app.use("/users", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`${PORT}번에서 대기중...`);
+});
