1cc76a15be945ca023dbc7ca0f3c7fe314069c9b
gzn_jjw <jjw_0418@naver.com>
2022-01-21 16:37:51 +0900

    feat: set up the app.js
     - package install
     - middlewares register
     - routing setup
     - 404 and 500 error handling
     :alarm_clock: webSocket

---
+++ practice_book/ch12/app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const path = require("path");
+const app = express();
+
+// websocket middleware
+const webSocket = require("./socket");
+
+// PORT
+const PORT = 8080;
+
+// routing name
+const indexRouter = require("./routes/index");
+
+dotenv.config();
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+// routing
+app.use("/", indexRouter);
+
+// 404 error
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없음`);
+  error.status = 404;
+  next(error);
+});
+
+// 500 error
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+const server = app.listen(PORT, () => {
+  console.log(PORT, "번에서 대기중...");
+});
+
+webSocket(server);
