a91b522e58f94cb4c7676d87bb6ab24ce143fe63
nwnp <jjw_0418@naver.com>
2022-02-10 17:52:09 +0900

    chore: 일부 수정

--- TodoList/routes/page.js
+++ TodoList/routes/page.js
@@ -5,9 +5,6 @@ const router = express.Router();
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
-  res.locals.followerCount = 0;
-  res.locals.followingCount = 0;
-  res.locals.followerIdList = [];
   next();
 });
 
