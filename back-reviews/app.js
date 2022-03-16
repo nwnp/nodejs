317c90a524c52d937081b4da9cd89d193a1a4559
nwnp <jjw_0418@naver.com>
2022-03-17 02:02:14 +0900

    feat: 화면 구성 및 라우트(rendering)

--- back-reviews/app.js
+++ back-reviews/app.js
@@ -1,2 +1,47 @@
-const express = require('express')
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
 
+const pageRouter = require("./routes/page.js");
+
+const app = express();
+const PORT = 8080;
+
+dotenv.config();
+
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+app.use("/", pageRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(PORT, "번에서 대기 중");
+});
