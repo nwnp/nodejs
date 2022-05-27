5b1c76229b3db128d4fdf72421cd8f6e3c4a1b42
nwnp <jjw_0418@naver.com>
2022-05-28 00:19:31 +0900

    practice multer how to use

--- pr-nodejs/part1/src/app2.js
+++ pr-nodejs/part1/src/app2.js
@@ -1,21 +1,26 @@
 // @ts-check
 
 const express = require("express");
+const multer = require("multer");
 
 const app = express();
 const PORT = 8080;
 
+// to use multer
+const upload = multer({ dest: "uploads/" });
+
 const USERS = {
   15: {
     nickname: "foo",
+    profileImageKey: undefined,
   },
   16: {
     nickname: "bar",
+    profileImageKey: undefined,
   },
 };
 
 app.use(express.json());
-app.use("/public", express.static("src/public"));
 
 app.set("views", "src/views"); // src 폴더 안에 viwes폴더를 추적할 수 있게 설정
 app.set("view engine", "pug");
@@ -54,8 +59,21 @@ app.get("/:id", (req, res, next) => {
     res.render("user-profile", {
       // @ts-ignore
       nickname: req.user.nickname, // index.pug 파일에 변수로 넘겨줌
+      userId: req.params.id,
+      message: "profile image upload 해보기",
+      userProfileImageURL: `/uploads/${req.user.profileImageKey}`,
     });
   }
+});
+app.use("/public", express.static("src/public"));
+app.use("/uploads", express.static("uploads"));
+
+// to use multer
+app.post("/:id/profile", upload.single("profile"), (req, res, next) => {
+  const { user } = req;
+  const { filename } = req.file;
+  user.profileImageKey = filename;
+  res.send(`User profile uploaded: ${filename}`);
 });
 
 // error-handling
